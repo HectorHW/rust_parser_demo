2b209cc9bc09d6acb599d0a14905b38d2c8b4a5c
HectorHW <hector.hanshew@gmail.com>
2021-05-05 15:22:19 +0300

    comments (single and multiline)

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,6 @@
 use std::fmt::{Display, Formatter};
 use std::fmt;
+use std::option::Option::Some;
 
 pub enum Token{
     Op(char),
@@ -40,7 +41,34 @@ pub fn tokenize(input:&str) -> Result<Vec<Token>, String>{
     while let Some(pair) = iterator.peek() {
         let c = (*pair).1;
         match c {
-            '+' | '-'| '*'|'/' => {res.push(Op(c)); iterator.next();}
+            '+' | '-'| '*' => {res.push(Op(c)); iterator.next();}
+
+            '/' => {
+                //division or comment
+                iterator.next(); //consume it
+                match iterator.peek() {
+                    Some((_, '/')) => {
+                        //comment
+                        while let Some(pair) = iterator.next() { //skip until EOL
+                            if pair.1=='\n' {break;}
+                        }
+                    }
+
+                    Some((_, '*')) => { // start of multiline comment. /*
+                        //skip until */
+                        let mut ended_flag = false;
+                        while let (Some(p1), Some(p2)) = (iterator.next(), iterator.peek())
+                        {
+                            if p1.1=='*' && p2.1=='/' {ended_flag=true; break;}
+                        }
+
+                        if !ended_flag {return Err("unterminated multiline comment".to_string())}
+                        iterator.next(); //consume /
+                    }
+
+                    _ => {res.push(Op('/'))}
+                }
+            }
 
             '(' => {res.push(LBracket); iterator.next();}
             ')' => {res.push(RBracket); iterator.next();}
