6c842a039992b60101d99d539d358f24ffeddfeb
HectorHW <hector.hanshew@gmail.com>
2021-05-06 11:51:50 +0300

    token indexation; error reporting with synchronization

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,33 +1,79 @@
 use std::fmt::{Display, Formatter};
 use std::fmt;
 use std::option::Option::Some;
+#[derive(Copy, Clone)]
+pub struct TokenIndex {
+    pub index: usize,
+    pub line_number:usize
+}
+
+impl Display for TokenIndex{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "[{},{}]", self.line_number, self.index)
+    }
+}
+
+pub const MOCK_IDX: TokenIndex = TokenIndex{index:0,line_number:0};
+
 
 pub enum Token{
-    Op(char),
-    Number(i32),
-    LBracket,
-    RBracket,
-    Print,
-    Var,
-    Equals,
-    Identifier(String),
-    Semicolon,
-    EOF
+    Op(char, TokenIndex),
+    Number(i32, TokenIndex),
+    LBracket(TokenIndex),
+    RBracket(TokenIndex),
+    Print(TokenIndex),
+    Var(TokenIndex),
+    Equals(TokenIndex),
+    Identifier(String, TokenIndex),
+    Semicolon(TokenIndex),
+    EOF(TokenIndex)
+}
+
+impl Token {
+    pub fn get_pos(&self) -> TokenIndex {
+        *match self {
+            Token::Op(_, r) => {r}
+            Token::Number(_, r) => {r}
+            Token::LBracket(r) => {r}
+            Token::RBracket(r) => {r}
+            Token::Print(r) => {r}
+            Token::Var(r) => {r}
+            Token::Equals(r) => {r}
+            Token::Identifier(_, r) => {r}
+            Token::Semicolon(r) => {r}
+            Token::EOF(r) => {r}
+        }
+    }
+
+    pub fn get_token_type_name(&self) -> String {
+        match self {
+            Token::Op(..) => {"Binary operator"}
+            Token::Number(..) => {"Number"}
+            Token::LBracket(_) => {"("}
+            Token::RBracket(_) => {")"}
+            Token::Print(_) => {"print keyword"}
+            Token::Var(_) => {"var keyword"}
+            Token::Equals(_) => {"equals"}
+            Token::Identifier(..) => {"identifier"}
+            Token::Semicolon(_) => {"semicolon"}
+            Token::EOF(_) => {"EOF"}
+        }.to_string()
+    }
 }
 
 impl Display for Token {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "{}", match self {
-            Token::Op(c) => {format!("<operator {}>", c)}
-            Token::Number(n) => {format!("<Number {}>", n)}
-            Token::LBracket => {"<(>".to_string()}
-            Token::RBracket => {"<)>".to_string()}
-            Token::Print => {"<print>".to_string()}
-            Token::Equals => {"<=>".to_string()}
-            Token::Identifier(name) => {format!("<variable {}>", name)}
-            Token::Semicolon => {"<;>".to_string()}
-            Token::Var => {"<var>".to_string()}
-            Token::EOF => {"<EOF>".to_string()}
+            Token::Op(c, r) => {format!("<operator {} [{},{}]>", c, r.line_number, r.index)}
+            Token::Number(n, r) => {format!("<Number {} [{},{}]>", n, r.line_number, r.index)}
+            Token::LBracket(r) => {format!("<( [{}, {}]>", r.line_number, r.index)}
+            Token::RBracket(r) => {format!("<) [{}, {}]>", r.line_number, r.index)}
+            Token::Print(r) => {format!("<print [{}, {}]>", r.line_number, r.index)}
+            Token::Equals(r) => {format!("<= [{}, {}]>", r.line_number, r.index)}
+            Token::Identifier(name, r) => {format!("<variable {} [{},{}]>", name, r.line_number, r.index)}
+            Token::Semicolon(r) => {format!("<; [{},{}]>", r.line_number, r.index)}
+            Token::Var(r) => {format!("<var [{},{}]>", r.line_number, r.index)}
+            Token::EOF(r) => {format!("<EOF [{}, {}]>", r.line_number, r.index)}
         })
     }
 }
@@ -36,12 +82,30 @@ pub fn tokenize(input:&str) -> Result<Vec<Token>, String>{
 
     use Token::*;
 
+    let mut line_start = 0;
+    let mut line_number = 0;
+
+    fn index(index:usize, line_number:usize) -> TokenIndex{
+        TokenIndex{index, line_number}
+    }
+
+    fn current_index(absolute_idx: usize, line_start:usize, line_number:usize) -> TokenIndex {
+        index(absolute_idx-line_start, line_number)
+    }
+
+    fn update_newline(absolute_idx:usize, line_start:&mut usize, line_number:&mut usize) {
+        *line_number += 1;
+        *line_start = absolute_idx+1;
+    }
+
+
     let mut iterator = input.char_indices().peekable();
     let mut res:Vec<Token> = Vec::new();
     while let Some(pair) = iterator.peek() {
         let c = (*pair).1;
+        let absolute_idx = (*pair).0;
         match c {
-            '+' | '-'| '*' => {res.push(Op(c)); iterator.next();}
+            '+' | '-'| '*' => {res.push(Op(c, current_index(pair.0, line_start, line_number))); iterator.next();}
 
             '/' => {
                 //division or comment
@@ -50,15 +114,17 @@ pub fn tokenize(input:&str) -> Result<Vec<Token>, String>{
                     Some((_, '/')) => {
                         //comment
                         while let Some(pair) = iterator.next() { //skip until EOL
-                            if pair.1=='\n' {break;}
+                            if pair.1=='\n' {update_newline(pair.0, &mut line_start, &mut line_number);break;}
                         }
                     }
 
                     Some((_, '*')) => { // start of multiline comment. /*
                         //skip until */
                         let mut ended_flag = false;
+                        let start = current_index(absolute_idx, line_start, line_number);
                         while let (Some(p1), Some(p2)) = (iterator.next(), iterator.peek())
                         {
+                            if p1.1=='\n' {update_newline(p1.0, &mut line_start, &mut line_number);}
                             if p1.1=='*' && p2.1=='/' {ended_flag=true; break;}
                         }
 
@@ -66,14 +132,15 @@ pub fn tokenize(input:&str) -> Result<Vec<Token>, String>{
                         iterator.next(); //consume /
                     }
 
-                    _ => {res.push(Op('/'))}
+                    _ => {res.push(Op('/', current_index(absolute_idx, line_start, line_number)))}
                 }
             }
 
-            '(' => {res.push(LBracket); iterator.next();}
-            ')' => {res.push(RBracket); iterator.next();}
+            '(' => {res.push(LBracket(current_index(absolute_idx, line_start, line_number))); iterator.next();}
+            ')' => {res.push(RBracket(current_index(absolute_idx, line_start, line_number))); iterator.next();}
 
             _ if isnum(c) => {
+                let start_idx = current_index(absolute_idx, line_start, line_number);
                 let mut num = String::new();
                 num.push(c);
                 iterator.next();
@@ -88,12 +155,13 @@ pub fn tokenize(input:&str) -> Result<Vec<Token>, String>{
                     }
                 }
 
-                res.push(Number(str::parse::<i32>(&*num).unwrap()));
+                res.push(Number(str::parse::<i32>(&*num).unwrap(), start_idx));
 
             }
 
             _ if isalpha(c) => {
                 let mut token = String::new();
+                let start_idx = current_index(absolute_idx, line_start, line_number);
                 token.push(c);
                 iterator.next();
 
@@ -108,28 +176,32 @@ pub fn tokenize(input:&str) -> Result<Vec<Token>, String>{
 
                 match token.as_str() {
                     "print" => {
-                        res.push(Print);
+                        res.push(Print(start_idx));
                     }
                     "var" => {
-                        res.push(Var);
+                        res.push(Var(start_idx));
                     }
 
                     _ => {
-                        res.push(Identifier(token));
+                        res.push(Identifier(token, start_idx));
                     }
                 }
 
             }
 
-            '=' => {res.push(Equals); iterator.next();}
-            ';' => {res.push(Semicolon); iterator.next();}
+            '=' => {res.push(Equals(current_index(absolute_idx, line_start, line_number))); iterator.next();}
+            ';' => {res.push(Semicolon(current_index(absolute_idx, line_start, line_number))); iterator.next();}
 
-            _ if c==' '|| c=='\n'|| c=='\t'||c=='\r' => {iterator.next();}
+            _ if c==' '|| c=='\t'||c=='\r' => {iterator.next();}
+            _ if c=='\n' => {
+                update_newline(absolute_idx, &mut line_start, &mut line_number);
+                iterator.next();
+            }
 
             _ => {return Err(format!("unknown character {} at {}", c, (*pair).0))}
         }
     }
-    res.push(Token::EOF);
+    res.push(Token::EOF(current_index(input.len(), line_start, line_number)));
     return Ok(res);
 }
 
